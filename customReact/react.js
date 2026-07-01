a013afdb9693bb2796c6d918d736bf816daf5fe2
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-01 15:36:59 +0000

    .

---
+++ customReact/react.js
@@ -0,0 +1,32 @@
+
+// function customRender(reactEl, container) {
+//     const domEl = document.createElement(reactEl.type);
+//     domEl.innerHTML = reactEl.children;
+
+//     domEl.setAttribute("href", reactEl.props.href)
+//     domEl.setAttribute("target", reactEl.props.target)
+
+//     container.appendChild(domEl);
+// }
+
+//alternative - better 
+const domEl = document.createElement(reactEl.type);
+domEl.innerHTML = reactEl.children;
+for (const prop in reactEl.props) {
+    if (prop === "children") continue;
+    domEl.setAttribute(prop,reactEl.props[prop])
+}
+container.appendChild(domEl);
+
+const reactEl = {
+    type: 'a',
+    props: {
+        href: "https://www.google.com",
+        target: "_blank"
+    },
+    children: "click me to visit google"
+}
+
+
+const mainContainer = document.getElementById('root');
+customRender(reactEl, mainContainer)
