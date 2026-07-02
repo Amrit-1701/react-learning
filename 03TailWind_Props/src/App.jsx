ebb88248f71f8a982c9638c19cb4758abaefceb4
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-02 08:49:41 +0000

    .

--- 03TailWind_Props/src/App.jsx
+++ 03TailWind_Props/src/App.jsx
@@ -1,13 +1,20 @@
-import React,{ useState } from 'react'
-import './index.css'
-import './App.css'
+import { useState } from 'react'
 
-function App() {
-  
+import  './App.css'
+import Card from './components/card'
+
+//props is a way to pass data from parent component to child component.
+//  It is a way to make components reusable and dynamic. Props are read-only, 
+// which means that they cannot be modified by the child component.
+
+function App(props) {
+  console.log('props',props)
 
   return (
     <>
-     <h1 className="bg-green-400 text-black">Tailwind Test</h1>
+     <h1 className='bg-red-500 text-white p-4 rounded-xl'>Tailwind Test</h1>
+     <Card channel="John Doe" /> {/* the value I write in this will show in props */}
+    <Card />
     </>
   )
 }
