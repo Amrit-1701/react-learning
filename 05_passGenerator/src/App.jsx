9840da23ad3747167fc21e59f9513b96893c97d9
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-03 10:05:23 +0000

    .

--- 05_passGenerator/src/App.jsx
+++ 05_passGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 
 import './App.css'
 
@@ -18,21 +18,73 @@ function App() {
 
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      
-      pass = str.charAt(char)
 
-
+      pass += str.charAt(char)
 
     }
     setPass(pass)
 
-
   }, [length, numAllow, charAllow, setPass])
 
+
+  useEffect(() => {
+    passGenerator()
+  }, [length, numAllow, charAllow, passGenerator])
+
+  
   return (
     <>
-      <h1 className="text-4xl text-center py-4 text-white">
-        Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800 ">
+        <h1 className="text-white text-center my-3 ">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={pass}
+            placeholder="Your Password"
+            readOnly
+            className="outline-none w-full py-1 px-3"
+          />
+          <br></br>
+          <button className="bg-pink-500 text-black rounded px-3 py-0.5 shrink-0">copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className='flex item-center gap-x-1'>
+            <input
+              type='range'
+              min="6"
+              max="50"
+              value={length}
+              className='cursor-pointer '
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className='flex item-center gap-x-1'>
+            <input
+              type='checkbox'
+              defaultChecked={numAllow}
+              id="numInput"
+              onChange={() => {
+                setNumAllow((prev) => !prev)
+              }}
+            />
+            <label htmlFor='numInput'>Numbers</label>
+
+          </div>
+          <div className='flex item-center gap-x-1'>
+            <input
+              type='checkbox'
+              defaultChecked={charAllow}
+              id="charInput"
+              onChange={() => {
+                setCharAllow((prev) => !prev)
+              }}
+            />
+            <label htmlFor='charInput'>Characters</label>
+          </div>
+        </div>
+
+      </div >
     </>
   )
 }
