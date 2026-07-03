584ab788d7246f13ec2a461ffc513a4321f5825c
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-03 09:23:08 +0000

    .

--- 05_passGenerator/src/App.jsx
+++ 05_passGenerator/src/App.jsx
@@ -1,9 +1,33 @@
-import { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numAllow, setNumAllow] = useState(false)
+  const [charAllow, setCharAllow] = useState(false)
+  const [pass, setPass] = useState('')
+
+  //const pass = useCallback(() => {function body} , [dependency])
+  const passGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (numAllow) str += "0123456789"
+    if (charAllow) str += "!@#$%^&*()_+~`|}{[]:;?><,./-="
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      
+      pass = str.charAt(char)
+
+
+
+    }
+    setPass(pass)
+
+
+  }, [length, numAllow, charAllow, setPass])
 
   return (
     <>
