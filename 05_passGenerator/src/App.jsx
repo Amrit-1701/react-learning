455fc1b328b059cf469ed9f576b5b17449608f5a
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-04 06:40:13 +0000

    .

--- 05_passGenerator/src/App.jsx
+++ 05_passGenerator/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
     passGenerator()
   }, [length, numAllow, charAllow, passGenerator])
 
-  
+
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800 ">
