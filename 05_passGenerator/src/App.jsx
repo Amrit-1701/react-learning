94c26a207f5cb9418c03ad200f803881f3717b5d
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-04 06:40:24 +0000

    .

--- 05_passGenerator/src/App.jsx
+++ 05_passGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useRef, useEffect, useState, useCallback } from 'react'
 
 import './App.css'
 
@@ -7,6 +7,17 @@ function App() {
   const [numAllow, setNumAllow] = useState(false)
   const [charAllow, setCharAllow] = useState(false)
   const [pass, setPass] = useState('')
+
+  //useRef Hook
+  const passRef = useRef(null)
+
+
+  const copyPass2ClipBoard = useCallback(()=>{
+    passRef.current?.select()
+    passRef.current?.setSelection(0,999)
+    window.navigator.clipboard.writeText(pass)
+  },[pass]  )
+
 
   //const pass = useCallback(() => {function body} , [dependency])
   const passGenerator = useCallback(() => {
@@ -43,19 +54,23 @@ function App() {
             placeholder="Your Password"
             readOnly
             className="outline-none w-full py-1 px-3"
+            ref={passRef}
           />
-          <br></br>
-          <button className="bg-pink-500 text-black rounded px-3 py-0.5 shrink-0">copy</button>
+        
+          <button onClick={copyPass2ClipBoard}
+           className="bg-pink-500 text-black rounded px-3 py-0.5 shrink-0">copy</button>
+        
         </div>
         <div className="flex text-sm gap-x-2">
           <div className='flex item-center gap-x-1'>
             <input
               type='range'
-              min="6"
+              min="8"
               max="50"
               value={length}
               className='cursor-pointer '
               onChange={(e) => setLength(e.target.value)}
+              
             />
             <label>Length: {length}</label>
           </div>
