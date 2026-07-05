bd1033085a067bb69f4e263e910c887f629c54d9
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-05 12:38:34 +0000

    .

--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+import {Route,RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import App from './App.jsx'
 import Layout from './layout.jsx'
 import Header from './components/Header/header.jsx'
@@ -38,7 +38,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />} >
       <Route path='' element={<Home/>}></Route>
-      <Route path='' element={<Home/>}></Route>
+      <Route path='about' element={<About/>}></Route>
+      <Route path='contact' element={<Contact/>}></Route>
     </Route>
 
   )
