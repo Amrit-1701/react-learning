8174701e14ef4e2315a239440e27bbb9fd63a080
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-06 12:18:17 +0000

    .

---
+++ 08_ContextAPI/src/components/login.jsx
@@ -0,0 +1,27 @@
+import React, { useContext, useState } from 'react'
+import UserContext from '../context/UserContext'
+
+
+function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    
+
+    const handleSubmit = () => {
+
+    }
+
+    return (
+        <>
+            <div>
+                <h2>Login</h2>
+                <input value={username} type='text' onChange={(e)=>setUsername(e.target.value)} placeholder='username'></input>
+                <input type='text' value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='password'></input>
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+        </>
+    )
+}
+
+export default Login
