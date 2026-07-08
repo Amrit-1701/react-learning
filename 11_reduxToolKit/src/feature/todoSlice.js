494eee8f5565ffd4d0f46ce84b0b09429cf2a1b1
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-08 12:41:13 +0000

    todoslice

---
+++ 11_reduxToolKit/src/feature/todoSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{ id: 1, text: 'hello world' }],
+}
+
+//big version of reducer
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo) //Push into array
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            const { id, text } = action.payload
+
+            const todo = state.todo.find((todo) => todo.id === id)
+            if (todo) {
+                todo.text = text
+            }
+        }
+    },
+})
+
+//addtodo always provide state(current state of recent initialSate) and action(provide passed data values ) 
+//Reducers are simply functions that change the state.
+
+export const {} =todoSlice.actions
