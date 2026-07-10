16f64ac9dbc7a61424134ef54cd8c387abb674c7
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-10 10:25:48 +0000

    appwrite

---
+++ 12_MainApp/src/appwrite/1_auth.js
@@ -0,0 +1,8 @@
+import config from "../conf/config";
+import { Client, Account, ID } from 'appwrite'
+
+export class AuthService{}
+
+const authService = new AuthService()
+
+export default authService
