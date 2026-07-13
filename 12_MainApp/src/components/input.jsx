d1bca61c59ec89e968b2f157b4b9f37488987554
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-13 09:06:03 +0000

    .

--- 12_MainApp/src/components/input.jsx
+++ 12_MainApp/src/components/input.jsx
@@ -6,7 +6,20 @@ const Input = React.forwardRef(function Input({
     className = '',
     ...props
 }, ref) {
-    return <h1>Test</h1>
+    const id = useId()
+    return (
+        <div className='w-full'>
+            {label && <label className='inline block mb-1' htmlFor={id}>{label}</label>
+            }
+            <input
+            type={type}
+             className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+             ref={ref}
+             {...props}
+             id={id}
+            />
+        </div>
+    )
 })
 
 export default Input
