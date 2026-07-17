45b3cc326fd02aa1f93555ca17e23c54cf86d48b
Amrit-1701 <jaindyanamrit@gmail.com>
2026-07-17 15:50:01 +0000

    .

--- 12_MainApp/src/components/select.jsx
+++ 12_MainApp/src/components/select.jsx
@@ -10,7 +10,20 @@ function Select({
 
     return (
         <div className='w-full'>
-            {label && <label />}
+            {label && <label htmlFor={id} className=''></label>}
+            <select
+                {...props}
+                id={id}
+                ref={ref}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border-gray-200 w-full ${className}`}
+            >
+                {options?.map((options) => (
+                    <options key={options} value={options}>
+                        {options}
+                    </options>
+                ))}
+
+            </select>
         </div>
     )
 }
